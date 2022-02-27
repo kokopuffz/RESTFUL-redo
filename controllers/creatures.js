bd14627ef4ebcf5f881af3aa7f34f822e2b2820b
kokopuffz <chanheeya@gmail.com>
2022-02-27 03:17:16 -0600

    dinosaur moved to controllers, creatures controllers

--- controllers/creatures.js
+++ controllers/creatures.js
@@ -1,13 +1,43 @@
 let express = require("express");
+const res = require("express/lib/response");
 const { append } = require("express/lib/response");
 let router = express.Router();
 const fs = require("fs");
 
-router.get('/',(rec,res)=>{
+router.get('/',(req,res)=>{
   let creatures=fs.readFileSync('./prehistoric_creatures.json')
   let creaturesData=JSON.parse(creatures)
-  console.log(creaturesData)
+
+  let nameFilter=req.query.nameFilter
+  if (nameFilter) {
+    //filter out all the creatures who dont have queried name
+    creaturesData=creaturesData.filter(creature=>{
+      return creature.name.toLowerCase() === nameFilter.toLowerCase()
+    })
+  }
+  res.render('./creatures/index.ejs', {myCreatures: creaturesData})
 })
 
+router.get("/new", (req, res) => {
+  res.render("./creatures/new.ejs");
+});
+
+router.get('/:idx',(req,res)=>{
+  let creatures = fs.readFileSync("./prehistoric_creatures.json");
+  let creaturesData = JSON.parse(creatures);
+  let creatureIdx = req.params.idx
+  res.render('./creatures/show.ejs', {creature: creaturesData[creatureIdx]})
+})
+
+router.post('/',(req, res)=>{
+  let creatures = fs.readFileSync("./prehistoric_creatures.json");
+  let creaturesData = JSON.parse(creatures);
+
+  //add item to creature to array
+  creaturesData.push(req.body);
+  //save creature to the data.json file
+  fs.writeFileSync("./creatures.json", JSON.stringify(creaturesData));
+  res.redirect("./creatures");
+})
 
 module.exports=router
